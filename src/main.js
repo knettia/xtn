f7454a2f4de2abd1e16174b747814e245893eaaa
Amelia Gäßler <146373382+knettia@users.noreply.github.com>
2023-12-25 02:07:29 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-// const fs = require('fs');
-
-// Specify the path to your JSON file
 const dialoguePath = 'path/to/your/file.json';
-let dialogueData = { };
-
-// Fetch the JSON file
+let dialogueData;
 fetch("res/dialogues.json")
   .then(response => {
     if (!response.ok) {
@@ -14,59 +9,125 @@ fetch("res/dialogues.json")
   })
   .then(jsonData => {
     dialogueData = jsonData;
-    // Now you can work with the JSON data
-    // console.log(jsonData);
+    console.log(jsonData);
   })
   .catch(error => {
     console.error('Error fetching JSON:', error);
   });
 
+let dialogueID = 1;
+let sentenceId = 0;
+
+let choosingOptions = false;
+let drawingSentence = false
 
 document.addEventListener('DOMContentLoaded', function () {
-    // fs.readFile(dialoguePath, 'utf8', (err, data) => {
-    //     if (err) {
-    //       console.error('Error reading JSON file:', err);
-    //       return;
-    //     }
-
-    //     dialogueData = JSON.parse(data);
-    //     console.log(jsonData);
-    // });
-
     var contentBox = document.getElementById('contentBox');
+    var buttonBox = document.getElementById('buttonBox');
     var maxElements = 25;
-    var isAddingText = false;
 
-    function addText(element, text, index) {
+    dialogueExecuter();
+
+    function dialogueExecuter() {
+        let dialogueElement = dialogueData[dialogueID - 1];
+
+        if (sentenceId >= dialogueElement.sentences.length) {
+            sentenceId = 0;
+            choosingOptions = true;
+
+            dialogueElement.options.forEach(option => {
+                addButton(option.text, option.endID);
+            });
+        } else {
+            drawingSentence = true;
+
+            var textElements = contentBox.getElementsByTagName('p');
+
+            if (textElements.length >= maxElements) {
+                contentBox.removeChild(textElements[0]);
+            }
+
+            var newTextElement = document.createElement('p');
+            newTextElement.textContent = '';
+            contentBox.appendChild(newTextElement);
+
+            addText(newTextElement, dialogueElement.sentences[sentenceId].text, 0, dialogueElement.sentences[sentenceId].speaker);
+
+            sentenceId++;
+        }
+    }
+
+    function optionsExecuter(index) {
+        choosingOptions = false;
+        dialogueID = index;
+        dialogueExecuter();
+        for (var i = 0; i < buttonBox.children.length + 1; i++) {
+            buttonBox.removeChild(buttonBox.children[0]);
+        }
+    }
+
+    function addButton(label, index) {
+        const button = document.createElement('button');
+        button.textContent = label;
+        button.addEventListener('click', function() {
+            optionsExecuter(index);
+        });
+        document.querySelector('.button-box').appendChild(button);
+    }
+    
+    const speakers = {
+        1: new Audio('res/audio/snd_wngdng7.wav'),
+        2: new Audio('res/audio/snd_floweytalk1.wav'),
+        3: new Audio('res/audio/err.wav'),
+        4: new Audio('res/audio/snd_txtpap.wav'),
+        5: new Audio('res/audio/snd_mtt1.wav'),
+        6: new Audio('res/audio/snd_floweytalk2.wav'),
+        0: new Audio('res/audio/snd_tem5.wav'),
+    };
+
+    function addText(element, text, index, speaker) {
       if (index < text.length) {
         element.textContent += text.charAt(index);
+        // TODO: implement sounds based
+        var audio;
+        switch(speaker) {
+            case 1:
+                audio = new Audio('res/audio/snd_wngdng7.wav');
+                break;
+            case 2:
+                audio = new Audio('res/audio/snd_floweytalk1.wav');
+                break;
+            case 3:
+                audio = new Audio('res/audio/err.wav');
+                break;
+            case 4:
+                audio = new Audio('res/audio/snd_txtpap.wav');
+                break;
+            case 5:
+                audio = new Audio('res/audio/snd_mtt1.wav');
+                break;
+            case 6:
+                audio = new Audio('res/audio/snd_floweytalk2.wav');
+                break;
+            case 0:
+                audio = new Audio('res/audio/snd_tem5.wav');
+                break;
+        } 
 
-        // var audio = new Audio('res/audio/err.wav'); // Replace 'path/to/sound.mp3' with the actual path to your sound file
-        // audio.play();
+        // audio.load();
+        audio.play();
 
         setTimeout(function () {
-          addText(element, text, index + 1);
+          addText(element, text, index + 1, speaker);
         }, 25); // Adjust the delay between characters
       } else {
-        isAddingText = false;
+        drawingSentence = false;
       }
     }
 
     document.addEventListener('keydown', function (event) {
-      if (event.key === 'Enter' && !isAddingText) {
-        var textElements = contentBox.getElementsByTagName('p');
-
-        if (textElements.length >= maxElements) {
-          // Remove the oldest element
-          contentBox.removeChild(textElements[0]);
-        }
-
-        var newTextElement = document.createElement('p');
-        newTextElement.textContent = '';
-        contentBox.appendChild(newTextElement);
-
-        isAddingText = true;
-        addText(newTextElement, 'New text element added!', 0);
+      if (event.key === 'Enter' && !choosingOptions && !drawingSentence) {
+        dialogueExecuter();
       }
     });
   });
